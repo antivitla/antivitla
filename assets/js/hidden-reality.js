ace1429c13175516b03b70d297abad3a7fa5b8aa
Виталий Кулешин <antivitla@gmail.com>
2017-02-10 22:07:36 +0300

    hidden reality

--- assets/js/hidden-reality.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var elements = document.querySelectorAll(".hidden-reality-reveal");
-  elements.forEach(function (element) {
-    element.addEventListener("click", function () {
-      document.querySelector("body").classList.toggle("hidden-reality-revealed");
-    });
-  });
-});
