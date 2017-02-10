f6bf20f3bdd2fa7c4a63bc501ad3033c768b3148
Виталий Кулешин <antivitla@gmail.com>
2017-02-10 22:04:33 +0300

    hidden reality

--- assets/js/hidden-reality.js
+++ assets/js/hidden-reality.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var elements = document.querySelectorAll(".hidden-reality-reveal");
   elements.forEach(function (element) {
-    element.style.cursor = "pointer";
     element.addEventListener("click", function () {
       document.querySelector("body").classList.toggle("hidden-reality-revealed");
     });
